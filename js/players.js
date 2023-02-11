774bd23cdbfef0406d9d7a7aca7d1371abf599e2
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-02-11 18:03:13 +0100

    Working on users token choice to play with

--- js/players.js
+++ js/players.js
@@ -1,5 +1,3 @@
-
-
 //Getting players name from Inputs
 
 let playerInputs = {
@@ -7,9 +5,6 @@ let playerInputs = {
     player2 : ''
 }
 let inputElements = Array.from(document.getElementsByTagName("input"));
-const storeNames = () => {
-    sessionStorage.setItem("playerNames", JSON.stringify(playerInputs));
-}
 
 inputElements.map(
     inputElement => {
@@ -19,7 +14,36 @@ inputElements.map(
                 if(inputElement.name == player){
                     playerInputs[player] = inputElement.value;
                 }
-                console.log(playerInputs)
+            }
+        })
+    }
+)
+
+// Getting token choice
+
+let tokens = {
+    tokenCocodrileP1 : "",
+    tokenElephantP1 : "",
+    tokenMonkeyP1 : "",
+    tokenRinoP1 : "",
+    tokenCocodrileP2 : "",
+    tokenElephantP2 : "",
+    tokenMonkeyP2 : "",
+    tokenRinoP2 : ""
+    }
+
+let tokenDivs = Array.from(document.getElementsByClassName("token"));
+
+tokenDivs.map(
+    tokenDiv => {
+        tokenDiv.addEventListener("click", ()=>{
+
+            for(let token in tokens){
+                if(tokenDiv.id == token){
+                    tokens[token] = tokenDiv.id;
+                    console.log(tokens[token])
+                    console.log(tokens)
+                }
             }
         })
     }
@@ -29,7 +53,7 @@ inputElements.map(
 let startBtn = document.getElementById("start-game-btn");
 // Function to store players names 
 
-const storePlayersNames = () => {
+const storePlayersInfo = () => {
 
     if( (playerInputs.player1 === '') || (playerInputs.player2 === '') ){
 
@@ -38,12 +62,18 @@ const storePlayersNames = () => {
     }
     sessionStorage.setItem('playersNames', JSON.stringify(playerInputs));
     window.location.href = "../pages/game.html";
-    console.log(JSON.stringify(playerInputs))
 
+    if( ((tokens.tokenCocodrileP1 === '') && (tokens.tokenElephantP1 === '') && (tokens.tokenMonkeyP1 === '') && (tokens.tokenRinoP1 === '')) || ((tokens.tokenCocodrileP2 === '') && (tokens.tokenElephantP2 === '') && (tokens.tokenMonkeyP2 === '') && (tokens.tokenRinoP2 === ''))){
+
+        return;
+
+    }
+    sessionStorage.setItem('tokenChoices', JSON.stringify(tokens));
+    window.location.href = "../pages/game.html";
 }
-console.log(JSON.stringify(playerInputs))
 
-startBtn.addEventListener("click", storePlayersNames);
+
+startBtn.addEventListener("click", storePlayersInfo);
 
 
 
--- js/winner.js
+++ js/winner.js
@@ -1,3 +1,14 @@
+// Adding span element where to show the names on the games page to a variable
+let nameP1Placeholder = document.querySelector(".player1-name-placeholder");
+let nameP2Placeholder = document.querySelector(".player2-name-placeholder");
+
+// Assigning the data from sessionStorage to a variable
+let playerNames = JSON.parse(sessionStorage.getItem('playersNames'));
+
+// Adding the text to the span element
+nameP1Placeholder.innerHTML = `${playerNames.player1.toUpperCase()}`;
+nameP2Placeholder.innerHTML = `${playerNames.player2.toUpperCase()}`;
+
 const showWinner = () => {
     let winnerPanelDiv = document.getElementById("winner-panel");
     let winnerPar = document.createElement("p")
